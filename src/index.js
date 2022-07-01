4a94b5cc5462f68cc36c8c902f6657c26b9a04be
Claire Thorington <claire.thorington@gmail.com>
2022-07-01 10:27:07 -0700

    WIP commit, setting up foundation

--- src/index.js
+++ src/index.js
@@ -3,27 +3,30 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-// $(document).ready(function() {
-//   $('#weatherLocation').click(function() {
-//     const city = $('#location').val();
-//     $('#location').val("");
+$(document).ready(function() {
+  $('#convert').click(function() {
+    const city = $('#amount').val();
+    $('#location').val("");
 
-//     let request = new XMLHttpRequest();
-//     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    let request = new XMLHttpRequest();
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}`;
 
-//     request.onreadystatechange = function() {
-//       if (this.readyState === 4 && this.status === 200) {
-//         const response = JSON.parse(this.responseText);
-//         getElements(response);
-//       }
-//     }
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
 
-//     request.open("GET", url, true);
-//     request.send();
+    request.open("GET", url, true);
+    request.send();
 
-//     function getElements(response) {
-//       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-//       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
-//     }
-//   });
-// });
+    function getElements(response) {
+      $('.showPesos').text(`The amount in Pesos is ${response.main.temp}`);
+      $('.showYen').text(`The amount in Yen is ${response.main.temp} .`);
+      $('.showRupees').text(`The amount in Rupees is ${response.main.temp} .`);
+      $('.showWon').text(`The amount in Won is ${response.main.temp} .`);
+      $('.showEuros').text(`The amount in Euros is ${response.main.temp} .`);
+    }
+  });
+});
