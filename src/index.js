6bd64ead629be4af56cafab928ead3d68e07db35
Claire Thorington <claire.thorington@gmail.com>
2022-07-02 21:18:23 -0700

    got it workinggit add .! kind of...

--- src/index.js
+++ src/index.js
@@ -26,7 +26,16 @@ $(document).ready(function () {
 
     function getElements(response) {
       if (response.conversion_rates) {
-        $('#showEuros').text(`The amount in Euros is € (${(userAmount)} * ${(response.conversion_rates.EUR)}`);
+        const euros = (((response.conversion_rates.EUR)*(userAmount)).toFixed(0));
+        const pesos = (((response.conversion_rates.MXN)*(userAmount)).toFixed(0));
+        const hkd = (((response.conversion_rates.HKD)*(userAmount)).toFixed(0));
+        const rupees = (((response.conversion_rates.INR)*(userAmount)).toFixed(0));
+        const krona = (((response.conversion_rates.SEK)*(userAmount)).toFixed(0));
+        $('.showEuros').text(`The amount in Euros is €` + euros);
+        $('.showPesos').text(`The amount in Mexican Pesos is $` + pesos);
+        $('.showHkDollars').text(`The amount in Hong Kong Dollars is $` + hkd);
+        $('.showRupees').text(`The amount in Indian Rupees is ₹` + rupees);
+        $('.showKrona').text(`The amount in Swedish Krona is kr` + krona);
       } else {
         $('.showErrors').text(`There was an error: ${response}`);
       }
